fe1023c28d99634db1ef8515417cb8cdac24de3c
Silas Meyer <silas.meyer@outlook.de>
2018-06-23 23:00:37 +0200

    References through AJAX

--- main.js
+++ main.js
@@ -1,51 +1,60 @@
 $(document).ready(function(){
-
-  $.getJSON( "https://www.silas229.de/api.php?projects=en", function( data ) {
-    var items = [];
-    $.each(data, function(i, object) {
-      $.each(object, function(kay, val) {
-          items.push( "<li id='" + key + "'>" + val + "</li>" );
-      });
+  if (typeof language !== 'undefined') {
+    $('.slider').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToScroll: 1,
+      slidesToShow: 3,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      draggable: false,
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            centerMode: false,
+            autoplay: false
+          }
+        },
+        {
+          breakpoint: 899,
+          settings: {
+            slidesToShow: 2,
+            arrows: false
+          }
+        },
+        {
+          breakpoint: 599,
+          settings: {
+            slidesToShow: 1,
+            arrows: false
+          }
+        }
+      ]
     });
-    $( "<ul/>", {
-      "class": "",
-      html: items.join("" )
-    }).appendTo( "body" );
-  });
 
-  $('.slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToScroll: 1,
-    slidesToShow: 3,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    draggable: false,
-    responsive: [
-      {
-        breakpoint: 1199,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          centerMode: false,
-          autoplay: false
+    $.getJSON( "https://www.silas229.de/api.php?projects="+language )
+    .done(function( data ) {
+      var projects = [];
+      var project;
+      $.each(data, function(i, object) {
+        if (object.newtab) {
+          object.url = 'https://silas229.de' + object.url;
         }
-      },
-      {
-        breakpoint: 899,
-        settings: {
-          slidesToShow: 2,
-          arrows: false
-        }
-      },
-      {
-        breakpoint: 599,
-        settings: {
-          slidesToShow: 1,
-          arrows: false
-        }
-      }
-    ]
-  });
+        project = '<a href="' + object.url +'" class="slider-element" target="_blank">';
+        project += '<img src="https://silas229.de/lib/img/' + object.icon_url + '-ico.png">';
+        project += '<br>';
+        project += '<span>' + object.tax + '</span>';
+        project += '<h1>' + object.name +'</h1>';
+        project += '<p>' + object.description +'</p>';
+        project += '</a>'
+        $('.slider').slick('slickAdd',project);
+      });
+      $(".slider").slick('removeSlide', 0);
+      //$(".slider").html(projects);
+    });
+  }
 });
