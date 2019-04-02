3367be67281276722005dc6964dc2741cd0370bf
Silas Meyer <contact@silas229.de>
2019-04-02 22:29:01 +0200

    version 2.0 + completely new design + new API + no more Silas_229, everywhere!

--- main.js
+++ main.js
@@ -1,131 +1,92 @@
-$(document).ready(function(){
-  if (typeof language !== 'undefined') {
-    $('.slider').slick({
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToScroll: 1,
-      slidesToShow: 3,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      draggable: false,
-      responsive: [
-        {
-          breakpoint: 1199,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            centerMode: false,
-            arrows: false
-          }
-        },
-        {
-          breakpoint: 899,
-          settings: {
-            slidesToShow: 2,
-            arrows: false
-          }
-        },
-        {
-          breakpoint: 599,
-          settings: {
-            slidesToShow: 1,
-            arrows: false
-          }
-        }
-      ]
-    });
+i18next
+	.use(i18nextXHRBackend)
+	.use(i18nextBrowserLanguageDetector)
+	.init({
+		fallbackLng: ["en", "de"],
+		debug: false,
+		backend: {
+			loadPath: 'https://api.silas229.de/v2/translations/portfolio/{{lng}}',
+			crossDomain: true
+		}
+	}, function(err, t) {
+		
+		localize = locI18next.init(i18next);
+		updateContent();
+		
+		i18next.on('languageChanged', () => {
+			updateContent();
+		});
+		
+	});
 
-    $.getJSON( "https://api.silas229.de/?projects="+language )
-    .done(function( data ) {
-      var project;
-      $.each(data, function(i, object) {
-        if (object.newtab == '0') {
-          object.url = 'https://www.silas229.de' + object.url;
-        }
-        project = '<a title="' + object.description  + '" href="' + object.url +'" class="slider-element" target="_blank">';
-        project += '<img src="https://cdn.silas229.de/projects/' + object.icon_url + '-ico.png">';
-        project += '<br>';
-        project += '<span>' + object.tax + '</span>';
-        project += '<h1 title="' + object.name  + '">' + object.name +'</h1>';
-        project += '<p title="' + object.description + '">' + object.description +'</p>';
-        project += '</a>'
-        $('.slider').slick('slickAdd',project);
-      });
-      $(".slider").slick('removeSlide', 0);
-      //$(".slider").html(projects);
-    });
+function updateContent() {
+	// localize = locI18next.init(i18next);
+	localize("body");
+	localize("#copyright", { date: new Date().getFullYear() });
+	updateSwitch();
+	updateProjects();
+}
 
-    const crate = new Crate({
-      server: '226768293195808768',
-      channel: '226774461599318017',
-      shard: 'https://cl2.widgetbot.io'
-    })
+function updateSwitch() {
+	var container = document.getElementsByClassName("languages")[0];
+	container.innerHTML = '';
+	i18next.languages.forEach(function(lng, i) {
+		container.innerHTML += `<span class="language${ ((lng == i18next.language) ? ' active' : '') }" onclick="i18next.changeLanguage('${ lng }')" title="${ lng }">${ lng }</span>`;
+	});
+}
 
-    if (language == 'de') { crate.notify( 'Hi!👋 Hinterlass doch eine Nachricht auf meinem Discord!😉' ) } else { crate.notify( "Hi!👋 Why don't you leave a message on my discord?😉" ) }
-    crate.on('signIn', data => {
-      console.log(`Gast als ${data.name} angemeldet`)
-    })
+function updateProjects() {
+	var wrapper = document.getElementsByClassName("projects")[0];
+	document.getElementsByClassName("loading")[0].style.display = "block";
+	wrapper.innerHTML = '<p class="loading" data-i18n="projects.loading">Loading...</p>';
+	localize(".loading");
+	
+	var request = new XMLHttpRequest();
+	request.open('GET', "https://api.silas229.de/v2/projects/" + i18next.language, true);
+	
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+			var projects = JSON.parse(request.responseText);
+			projects.forEach(function(project, i) {
+				wrapper.innerHTML += `
+          <a href="${ project.url }" class="project" target="_blank" title="${ project.name }" aria-label="${ project.name }">
+          <img src="https://cdn.silas229.de/projects/${ project.icon }.png" alt="" class="icon">
+          <h1 class="title">${ project.name }</h1>
+          <p class="description">${ project.description }</p>
+          <div class="tax"><div class="category">${ project.tax }</div></div>
+          </a>
+        `;
+			});
+			document.getElementsByClassName("loading")[0].style.display = "none"
+		} else {
+			console.log("Error while loading projects");
+		}
+	};
+	
+	request.send();
+}
 
-  }
+function openModal(modal) {
+	document.getElementById(modal).style.display = "block";
+}
+
+function closeModal(modal) {
+	document.getElementById(modal).style.display = "none";
+}
+
+document.getElementById("privacy").addEventListener("click", function(e) {
+	openModal(this.dataset.target);
+	e.preventDefault();
 });
 
-$(document).ready(function(){
-  if (typeof language !== 'undefined') {
-    $('.slider').slick({
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToScroll: 1,
-      slidesToShow: 3,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      draggable: false,
-      responsive: [
-        {
-          breakpoint: 1199,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            centerMode: false,
-            autoplay: false
-          }
-        },
-        {
-          breakpoint: 899,
-          settings: {
-            slidesToShow: 2,
-            arrows: false
-          }
-        },
-        {
-          breakpoint: 599,
-          settings: {
-            slidesToShow: 1,
-            arrows: false
-          }
-        }
-      ]
-    });
-
-    $.getJSON( "https://www.silas229.de/api.php?projects="+language )
-    .done(function( data ) {
-      var project;
-      $.each(data, function(i, object) {
-        if (object.newtab) {
-          object.url = 'https://silas229.de' + object.url;
-        }
-        project = '<a href="' + object.url +'" class="slider-element" target="_blank">';
-        project += '<img src="https://www.silas229.de/lib/img/' + object.icon_url + '-ico.png">';
-        project += '<br>';
-        project += '<span>' + object.tax + '</span>';
-        project += '<h1>' + object.name +'</h1>';
-        project += '<p>' + object.description +'</p>';
-        project += '</a>'
-        $('.slider').slick('slickAdd',project);
-      });
-      $(".slider").slick('removeSlide', 0);
-      //$(".slider").html(projects);
-    });
-  }
+[].forEach.call(document.getElementsByClassName("close"), function(i) {
+	i.addEventListener("click", function(e) {
+		closeModal(this.dataset.target);
+	});
 });
+
+window.onclick = function(event) {
+	if (event.target == document.getElementById("privacyModal")) closeModal("privacyModal");
+}
+
+if (window.location.search.match( /privacy/gi )) openModal("privacyModal");
